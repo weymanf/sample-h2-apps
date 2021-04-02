147a3fe9dd8ccad370ecff7723eb44ae3674a072
Weyman Fung <weymanf@vmware.com>
2021-04-02 15:07:25 -0700

    add node h2 server

---
+++ http2-node/index.js
@@ -0,0 +1,21 @@
+const http2 = require('http2');
+const fs = require('fs');
+
+const server = http2.createSecureServer({
+  key: fs.readFileSync(`${__dirname}/localcert/local.key`),
+  cert: fs.readFileSync(`${__dirname}/localcert/local.crt`)
+});
+server.on('error', (err) => console.error(err));
+
+server.on('stream', (stream, headers) => {
+  // stream is a Duplex
+  stream.respond({
+    'content-type': 'text/html; charset=utf-8',
+    ':status': 200
+  });
+  stream.end('<h1>Hello World</h1>');
+});
+
+const port = process.env["PORT"] || 3000
+console.log(`listening on port: ${port}`)
+server.listen(port);
